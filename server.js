af6954fdfda1a32bdeda618f754bfd9b6a5b46e0
Sathish Kumar N <162999084+sathish9117@users.noreply.github.com>
2024-10-24 21:22:49 +0530

    added cros

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
+// Use CORS middleware
+server.use(cors());
+
+// Set default middlewares (logger, static, cors, no-cache)
 server.use(middlewares);
+
+// Add your custom routes before JSON Server router
 server.use(router);
+
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
